f36cff6d706dade804a0d123ae3e265ab167c369
pedrostuart <pedrohsmoura897@gmail.com>
2026-08-12 22:05:01 -0300

    .;

--- src/autores/autores.controller.ts
+++ src/autores/autores.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, ParseIntPipe, Param } from '@nestjs/common';
 import { CreateAutorDto } from './dto/create-autor.dto';
 import { AutoresService } from './autores.service';
+import { Get } from '@nestjs/common';
 
 @Controller('autores')
 export class AutoresController {
@@ -9,5 +10,13 @@ export class AutoresController {
     criarAutor(@Body() createAutorDto: CreateAutorDto){
         return this.autoresService.criarAutor(createAutorDto)
     }
-
+    @Get()
+    exibir(){
+        return this.autoresService.exibir()
+    }
+    @Get(':id')
+    exibirPorId(@Param('id', ParseIntPipe) id: number){
+        return this.autoresService.exibirPorId(id)
+    }
+    
 }
--- src/autores/autores.service.ts
+++ src/autores/autores.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAutorDto } from './dto/create-autor.dto'; 
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import { DatabaseService } from 'src/database/database.service';
 @Injectable()
 export class AutoresService {
@@ -24,5 +24,21 @@ export class AutoresService {
                 ano_nascimento
             }
         }
+    }
+    async exibir(){
+        // o await serve pra dar um tempo para ele buscar essas informações
+        const resultado = await this.databaseService.query(
+        'SELECT * FROM autor'
+        )
+        return resultado
+    }
+    async exibirPorId(id:number){
+        const resultado = await this.databaseService.query('SELECT * FROM autor WHERE id = ?', [id]) as RowDataPacket[]
+
+        if (resultado.length === 0){
+            throw new NotFoundException('Id não encontrado')
+        }
+
+        return resultado[0]
     }
 }
